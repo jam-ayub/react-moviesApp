215f8ef8cfe1cee9f6194b0c64335343c106b974
Jam <jamayub098@gmail.com>
2024-08-04 22:28:31 +0500

    unique keys added to tableBody component

--- src/components/fakeMovies.jsx
+++ src/components/fakeMovies.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
-import { genres, getGenres } from "../services/fakeGenreService";
+import { getGenres } from "../services/fakeGenreService";
 import ListGroup from "./common/listGroup";
 import Pagination from "./common/pagination";
 import { paginate } from "./utils/paginate";
