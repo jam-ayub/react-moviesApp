2e2b6e0c1c15f060dc8fbe8ba3ec371e3cf48e72
Jam <jamayub098@gmail.com>
2024-08-13 09:01:32 +0500

    routing added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import Movies from './components/fakeMovies';
+import React from "react";
+import logo from "./logo.svg";
+import { Route, Switch, Redirect } from "react-router-dom";
+import Movies from "./components/fakeMovies";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navBar";
+import MovieForm from "./components/movieForm";
+import "./App.css";
 
 function App() {
   return (
-    <main className='container'>
-      <Movies />
-    </main>
+    <React.Fragment>
+      <NavBar />
+      <main className="container">
+        <Switch>
+          <Route path="/movies/:id" component={MovieForm} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect exact from="/" to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    </React.Fragment>
   );
 }
 
