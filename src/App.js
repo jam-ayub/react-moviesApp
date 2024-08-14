3e2a280bebd417c39648ef003ee2976c62c0dcf1
Jam <jamayub098@gmail.com>
2024-08-14 10:37:49 +0500

    build a bootstrap form and handle form  submission

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import { Route, Switch, Redirect } from "react-router-dom";
 import Movies from "./components/fakeMovies";
 import Customers from "./components/customers";
@@ -7,7 +7,9 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound";
 import NavBar from "./components/navBar";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/common/loginForm";
 import "./App.css";
+
 
 function App() {
   return (
@@ -15,6 +17,7 @@ function App() {
       <NavBar />
       <main className="container">
         <Switch>
+          <Route path="/login" component={LoginForm} />
           <Route path="/movies/:id" component={MovieForm} />
           <Route path="/movies" component={Movies} />
           <Route path="/customers" component={Customers} />
