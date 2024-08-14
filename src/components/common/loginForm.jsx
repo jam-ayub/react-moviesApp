28782bda1dc9d2762f0776daf3aff23db44cccd6
Jam <jamayub098@gmail.com>
2024-08-14 13:04:56 +0500

    sync form elements with state objects

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -1,25 +1,49 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-    
-    handleSubmit = e => {
-        e.preventDefault();
+  state = {
+    account: { username: "", password: "" },
+  };
 
-        //call the server
-        console.log("submitted")
-    }
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log("Submitted");
+  };
+
+  handleChange = ({currentTarget: input}) => {
+    const account = { ...this.state.account };
+    account[input.name] = input.value;
+    this.setState({ account });
+  };
+
   render() {
+    const {account} = this.state; 
     return (
       <div>
         <h1>Login Here</h1>
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <lable htmlFor="username">Username</lable>
-            <input id="username" type="text" className="form-control" />
+            <input
+              value={account.username}
+              onChange={this.handleChange}
+              id="username"
+              name="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <lable htmlFor="password">Password</lable>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              name="password"
+              id="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
