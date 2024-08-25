45de1dcf598771e728a88a2459a05d8931fe90f9
Jam <jamayub098@gmail.com>
2024-08-25 23:14:43 +0500

    new movie form component added

--- src/components/fakeMovies.jsx
+++ src/components/fakeMovies.jsx
@@ -5,6 +5,7 @@ import ListGroup from "./common/listGroup";
 import Pagination from "./common/pagination";
 import { paginate } from "./utils/paginate";
 import MoviesTable from "./moviesTable";
+import { Link } from "react-router-dom";
 import _ from "lodash";
 
 class Movies extends Component {
@@ -105,6 +106,13 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
+          <Link
+            to="/movies/new"
+            className="btn btn-primary"
+            style={{ marginBottom: 20 }}
+          >
+            New Movie
+          </Link>
           <p>Displaying {totalCount} movies in the database.</p>
           <MoviesTable
             movies={movies}
