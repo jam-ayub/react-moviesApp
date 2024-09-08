9a6fd4723408487062d254195348aac137e8d782
Jam <jamayub098@gmail.com>
2024-09-08 18:56:48 +0500

    replaced fakeGenreService with genreService

--- src/components/fakeMovies.jsx
+++ src/components/fakeMovies.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { getMovies } from "../services/fakeMovieService";
-import { getGenres } from "../services/fakeGenreService";
+import { getGenres } from "../services/genreService";
 import ListGroup from "./common/listGroup";
 import Pagination from "./common/pagination";
 import { paginate } from "./utils/paginate";
@@ -21,8 +21,9 @@ class Movies extends Component {
     sortColumn: { path: "title", order: "acs" },
   };
 
-  componentDidMount() {
-    const genres = [{ _id: "", name: "All Movies" }, ...getGenres()];
+  async componentDidMount() {
+    const { data } = await getGenres();
+    const genres = [{ _id: "", name: "All Movies" }, ...data];
     this.setState({ movies: getMovies(), genres });
   }
 
