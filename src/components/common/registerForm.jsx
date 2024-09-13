4d6cc2ebcd51e69ce68fa46f0fd86db210f7bc85
Jam <jamayub098@gmail.com>
2024-09-13 11:50:37 +0500

    submitting login and register form

--- src/components/common/registerForm.jsx
+++ src/components/common/registerForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./form";
-
+import * as userService from "../../services/userService";
 class RegisterForm extends Form {
   state = {
     data: { username: "", password: "", name: "" },
@@ -14,9 +14,16 @@ class RegisterForm extends Form {
     name: Joi.string().required().label("Name"),
   };
 
-  doSubmit = () => {
-    // call the server
-    console.log("Submitted!");
+  doSubmit = async () => {
+    try {
+      await userService.register(this.state.data);
+    } catch (ex) {
+      if (ex.response && ex.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = ex.response.data;
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {
